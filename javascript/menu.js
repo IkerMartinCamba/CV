63ad67db38938d22eaf3dfd4d0ed3626595c69dd
Iker Martin <104889606+IkerMartinProg@users.noreply.github.com>
2023-12-24 19:54:58 +0100

    Add files via upload

---
+++ javascript/menu.js
@@ -0,0 +1,14 @@
+// Archivo JavaScript para el menú
+
+const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu-navegacion');
+
+hamburger.addEventListener('click',() => {
+	menu.classList.toggle('spread');
+})
+
+window.addEventListener('click', e => {
+	if (menu.classList.contains('spread') && e.target != menu && e.target != hamburger) {
+		menu.classList.toggle('spread');
+	}
+})
